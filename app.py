31b38c923fd1513eb08df9568a3892d1ae22328c
Deepak-1510 <62868543+Deepak-1510@users.noreply.github.com>
2026-04-27 12:18:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app= Flask(__name__)
 
 @app.route('/')
 def home():
-  return "Hello from devops Level 3"
+  return "WOwwwwwwwww!!!!!!!, it worked! Hello from devops Level 3"
 
 if  __name__ == "__main__":
   app.run(host='0.0.0.0', port=5000)
